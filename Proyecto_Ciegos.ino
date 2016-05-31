f54097addfc9357a90b724be6ab89c2df2b99fc5
yeffrimic <yeffrisalazar1@gmail.com>
2016-05-30 23:44:23 -0600

    second commit, first code try

--- Proyecto_Ciegos.ino
+++ Proyecto_Ciegos.ino
@@ -1,9 +1,41 @@
+
+#define iPinTrigger  1 //D1
+#define iPinEcho  2 //D2
+#define motor 0
 void setup() {
   // put your setup code here, to run once:
-
+ pinMode(iPinTrigger,OUTPUT);
+ pinMode(motor,OUTPUT);
+ pinMode(iPinEcho,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+digitalWrite(motor,HIGH);
+delay(ultrasonicsensor());
+digitalWrite(motor,LOW);
+delay(ultrasonicsensor());
+}
 
+int ultrasonicsensor() {
+
+  digitalWrite(iPinTrigger, LOW);
+  digitalWrite(iPinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(iPinTrigger, LOW);
+
+
+  //Read the data from the sensor
+  double fTime = pulseIn(iPinEcho, HIGH);
+  //convert to seconds
+  fTime = fTime / 1000000;
+  double fSpeed = 347.867;
+  //Get the distance in meters
+  double fDistance = fSpeed * fTime;
+  //Convert to centimeters;
+  fDistance = fDistance * 100;
+  //Get one way distance
+  fDistance = fDistance / 2;
+
+  delay(100);
+  return fDistance;
 }
